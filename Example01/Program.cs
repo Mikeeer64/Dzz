994ffa3477a3942ce63b2837a9a236e57cae7387
Mikeeer <my email>
2022-12-08 13:22:10 +0300

    Example01

---
+++ Example01/Program.cs
@@ -0,0 +1,27 @@
+﻿int GetNamber(string message)
+{
+    int count = 0;
+    bool isCorrect = false;
+    while(!isCorrect)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out count)) isCorrect = true;
+        else Console.WriteLine("Ввели не число");
+    }
+    return count;
+}
+
+int GetDegree(int namber1, int namber2)
+{
+    int result = namber1;
+    for (int i = 0; i <= namber2; i++)
+    {
+        result = result * namber1;
+    }
+    return result;
+}
+
+int namber1 = GetNamber("Введите первое число: ");
+int namber2 = GetNamber("Введите второе число: ");
+
+Console.WriteLine(GetDegree(namber1, namber1));
