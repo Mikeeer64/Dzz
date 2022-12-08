cf4d45b28c66154d68619a8a4c50106ef59dcd53
Mikeeer <my email>
2022-12-08 13:22:19 +0300

    Example02

---
+++ Example02/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetNamber(string message)
+{
+    int count = 0;
+    bool isCorrect = false;
+    while(!isCorrect)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out count)) isCorrect = true;
+        else Console.WriteLine("Ввели не число");
+    }
+    return count;
+}
+
+int namber = GetNamber("Введите число: ");
+int sum = 0;
+    
+do
+{
+    sum = sum + namber % 10;
+    namber = namber / 10;
+}
+while(namber > 0);
+
+Console.WriteLine($"Сумма чисел = {sum}");
